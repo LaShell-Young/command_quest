d76b7469bd87e1436af3aac62e79f2baf616e756
lashell <lashellyoung15@gamil.com>
2024-04-17 17:25:05 -0500

    fix failing test

--- src/tests/gameBoard.test.tsx
+++ src/tests/gameBoard.test.tsx
@@ -1,19 +1,20 @@
 import React from "react";
-// import { render, screen } from "@testing-library/react"
-// import GameBoard from "../components/gameBoard"
+import { render, screen } from "@testing-library/react"
+import GameBoard from "../components/gameBoard"
+import gameMap from "../images/gameMap.jpg"
 
-// // investigate pre test setup and post test clean up
-// // beforeEach(() => {
-// //     // test setup
-// // })
+// investigate pre test setup and post test clean up
+// beforeEach(() => {
+//     // test setup
+// })
 
-// test('game map renders', () => {
-//     // Arrange
-//     render(<GameBoard/>);
+test('game map renders', () => {
+    // Arrange
+    render(<GameBoard/>);
 
-//     // Act
-//     screen.getByRole()
+    // Act
+    const mapImage = screen.getByRole('img', { name: "game map"});
 
-//     //Assert
-    
-// });
+    //Assert
+    expect(mapImage).toBeInTheDocument();
+});
